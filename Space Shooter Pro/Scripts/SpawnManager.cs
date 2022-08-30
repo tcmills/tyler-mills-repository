737ee41739d8472c2d68f58427bdda7d4a24ffc7
tcmills <56266592+tcmills@users.noreply.github.com>
2022-08-30 19:22:44 -0400

    Add files via upload

---
+++ Space Shooter Pro/Scripts/SpawnManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject[] _powerups;
+    [SerializeField]
+    private GameObject _enemyContainer;
+
+    private bool _stopSpawning = false;
+
+    public void StartSpawning()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerupRoutine());
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        while (!_stopSpawning)
+        {
+            GameObject newEnemy = Instantiate(_enemyPrefab, new Vector3(Random.Range(-8f, 8f), 7f, 0), Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+
+    IEnumerator SpawnPowerupRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        while (!_stopSpawning)
+        {
+            // Waits 8-12, 13 does not count
+            yield return new WaitForSeconds(Random.Range(8, 13));
+            GameObject newPowerup = Instantiate(_powerups[Random.Range(0, 3)], new Vector3(Random.Range(-8f, 8f), 7f, 0), Quaternion.identity);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
