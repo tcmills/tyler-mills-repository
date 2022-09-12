f0a5fc8b72cc44eff56aa86cceaca519048243bb
tcmills <56266592+tcmills@users.noreply.github.com>
2022-09-11 22:10:06 -0400

    Add files via upload

---
+++ Course Work/Spring 2021 - Junior Year/Unity/Project 4/CameraNav.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraNav : MonoBehaviour
+{
+    float RotationY = 0f;
+    float RotationX = 0f;
+    Vector3 oldPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 currentMouse = Input.mousePosition;
+
+        if (Input.GetMouseButton(1))
+        {
+            RotationX += Input.GetAxis("Mouse X") * 2;
+            RotationY += Input.GetAxis("Mouse Y") * -1 * 2;
+            Camera.main.transform.localEulerAngles = new Vector3(RotationY, RotationX);
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            oldPosition = currentMouse;
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            Vector3 newPosition = Camera.main.ScreenToViewportPoint(oldPosition - currentMouse);
+            Vector3 translation = new Vector3(newPosition.x * 2 * 2 * 2, newPosition.y * 2 * 2 * 2, 0);
+            Camera.main.transform.Translate(translation, Space.Self);
+            oldPosition = currentMouse;
+        }
+
+        Camera.main.transform.position += Input.GetAxis("Mouse ScrollWheel") * transform.forward * 2;
+
+    }
+}
---
+++ Course Work/Spring 2021 - Junior Year/Unity/Project 4/ObjectBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectBehavior : MonoBehaviour
+{
+
+    public Toggle translateToggle;
+    public Toggle rotateToggle;
+    public Toggle deleteToggle;
+    public Button addCube;
+    public GameObject cube;
+    public GameObject selected;
+    public float cameraDistanceZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateToggle.isOn = false;
+        deleteToggle.isOn = false;
+        translateToggle.isOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                selected = hit.transform.gameObject;
+                cameraDistanceZ = Camera.main.WorldToScreenPoint(selected.transform.position).z;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (translateToggle.isOn && selected != null)
+            {
+                Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraDistanceZ);
+                Vector3 newPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+                selected.transform.position = newPosition;
+            }
+            if (rotateToggle.isOn && selected != null)
+            {
+                selected.transform.RotateAround(Camera.main.transform.up, Input.GetAxis("Mouse X") * -0.3f);
+                selected.transform.RotateAround(Camera.main.transform.right, Input.GetAxis("Mouse Y") * 0.3f);
+            }
+            if (deleteToggle.isOn)
+            {
+                Destroy(selected);
+            }
+        } 
+        else
+        {
+            selected = null;
+        }
+    }
+
+    public void summonCube()
+    {
+        Instantiate(cube, Camera.main.transform.position + Camera.main.transform.forward * 5, Quaternion.identity);
+    }
+}
